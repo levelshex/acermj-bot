45eaf16add640cec878212f8ca4b4a2c45dc5c9c
Zhong Wei <cheezhongwei00@gmail.com>
2022-01-08 15:48:52 +0800

    "Fix /help command to properly print instructions to user"
    This reverts commit 9c9dd72f82bc91ac5c7bd1a5d974c5d1ad6992d3.

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@
 import logging
 import os
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CommandHandler
-from telegram import parse_mode
+from telegram import ParseMode
 from dbhelper import DBHelper
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
